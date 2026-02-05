4aee45dab4bd7cd4d06d61db3ea0278f1f19f35e
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-05 15:31:24 -0300

    feat(CORS): install dependency and implement middleware
    With:
    - dynamic origin handling
    - permission for requests without a known origin, for example: Postman
    - allowedHeaders
    - preflight configuration (OPTIONS)

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const mongoose = require('mongoose');
 
 const usersRouter = require('./routes/users');
 const articlesRouter = require('./routes/articles');
 
 const handleError = require('./middlewares/errorHandler');
+
+const ForbiddenError = require('./errors/ForbiddenError');
 
 // Pacote dotenv só lê .env., mas é possível especificar qual arquivo carregar
 // Ao rodar scripts (ou comandos), o Express vai pegar variáveis do NODE_ENV definido
@@ -16,6 +20,54 @@ require('dotenv').config({
 
 // Cria um aplicativo Express
 const app = express();
+
+// CORS
+
+// '.split(',')' para transformar a string em array
+// '.map()' com 'trim()' para remover qlqr espaço em branco que possa ter
+const allowedCors = process.env.CORS_ORIGIN.split(',').map((url) => url.trim());
+
+// Configuração com opções específicas
+const corsOptions = {
+  // O callback é uma função fornecida pelo middleware cors para indicar se a origem
+  // é permitida
+  // Espera dois parâmetros: callback(error, allow)
+  // error: null se não houve erro ou um objeto Error para bloquear
+  // allow: true se a origem é permitida ou false se não
+
+  origin: (origin, callback) => {
+    // Se não houver origin (Postman, curl, apps mobile), permite
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    // Se houver origin e estiver na lista, permite
+    if (allowedCors.includes(origin)) {
+      return callback(null, true);
+    }
+
+    // Caso contrário, bloqueia
+
+    // Cria um erro customizado com name
+    const corsError = new ForbiddenError(
+      `Origem não permitida pelo CORS, ${origin}`,
+    );
+    return callback(corsError);
+  }, // origens permitidas e tratamento com msg de erro, caso não
+
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // permite envio de cookies/autenticação (caso o projeto use
+  // cookies httpOnly, ao invés do armazenamento do JWT token no localStorage)
+};
+
+// Aplica CORS com opções específicas
+app.use(cors(corsOptions));
+
+// Trata requisições preflight (OPTIONS) para qualquer rota
+// Regex /.*/ para qlqr caminho, evita erro path-to-regex que ocorre com '*' ou '(.*)'
+// em versões recentes do Express
+app.options(/.*/, cors(corsOptions));
 
 // Para analisar application/json
 app.use(express.json());
