b5ad54d99c33fd86bf02ad72f7fc643dae6540fc
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-06 15:19:54 -0300

    feat(celebrate): add celebrateForSignin.js
    For loginUser validation.

---
+++ middlewares/celebrates/celebrateForSignin.js
@@ -0,0 +1,13 @@
+const { celebrate, Joi } = require('celebrate');
+
+const celebrateForSignin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string()
+      .required()
+      .min(8)
+      .pattern(/^[^<>]+$/), // regex para segurança básica, '<' e '>' não são permitidos
+  }),
+});
+
+export default celebrateForSignin;
