5f02114c5e5f455e3f5f03208d2e3db0a2717840
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-09 12:43:55 -0300

    fix(celebrate): align validation according to NewsAPI format
    Fields may return as null.

--- middlewares/celebrates/articles/celebrateForPost.js
+++ middlewares/celebrates/articles/celebrateForPost.js
@@ -2,15 +2,17 @@ const { celebrate, Joi } = require('celebrate');
 
 const celebrateForPost = celebrate({
   body: Joi.object().keys({
+    // Celebrate é configurado de acordo com as propriedades do obj que são definidas no
+    // front-end e não no schema do backend
     tag: Joi.string()
       .required()
       .pattern(/^[^<>]+$/), // regex para segurança básica, '<' e '>' não são permitidos
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    publishedAt: Joi.date().required(),
-    source: Joi.string().required(),
+    title: Joi.string().allow(null).optional(),
+    description: Joi.string().allow(null).optional(),
+    publishedAt: Joi.date().allow(null).optional(),
+    source: Joi.string().allow(null).optional(),
     url: Joi.string().uri().required(),
-    urlToImage: Joi.string().uri().required(),
+    urlToImage: Joi.string().uri().allow(null).optional(),
   }),
 });
 
