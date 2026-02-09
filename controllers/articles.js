a240c2ab6d9095e8fdf4bec8fe3adb8993fe4356
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-09 13:59:08 -0300

    fix(deleteUserArticles): correct parameter and add .select()
    - The route parameter was being passed as _id, but the route is defined in '/:articleId'
    - The owner field was not being returned with the find, so .select() was added

--- controllers/articles.js
+++ controllers/articles.js
@@ -66,11 +66,14 @@ const postUserArticles = async (req, res) => {
 // O manipulador de solicitação deleteUserArticles
 // Exclui o artigo armazenado pelo _id
 const deleteUserArticles = async (req, res) => {
-  // const { _id } = req.params;
+  // Como definido na rota '/:articleId'
+  // const { articleId } = req.params;
 
-  const articleToUnsave = await Articles.findById(req.params._id).orFail(() => {
-    throw new NotFoundError('Cadastro de artigo não encontrado');
-  });
+  const articleToUnsave = await Articles.findById(req.params.articleId)
+    .select('+owner') // para o campo retornar neste find
+    .orFail(() => {
+      throw new NotFoundError('Cadastro de artigo não encontrado');
+    });
 
   // Verifica se o usuário atual é um salvador do artigo atual para poder prosseguir os
   // próximos passos do controlador
@@ -84,14 +87,16 @@ const deleteUserArticles = async (req, res) => {
   // anteriormente), deleta o recurso todo
   if (articleToUnsave.owner.length === 1) {
     // Deleta o artigo da coleção
-    const deletedArticle = await Articles.findByIdAndDelete(req.params._id);
+    const deletedArticle = await Articles.findByIdAndDelete(
+      req.params.articleId,
+    );
 
     return res.send({ deletedArticle });
   }
 
   // Se houver mais de um salvamento, apenas atualiza o array de id de usuários
   const unsavedArticle = await Articles.findByIdAndUpdate(
-    req.params._id,
+    req.params.articleId,
     // Remove o _id do usuário do array owner
     { $pull: { owner: req.user._id } },
     { new: true, runValidators: true },
